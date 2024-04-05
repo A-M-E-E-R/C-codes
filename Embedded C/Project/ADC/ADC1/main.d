d1f9ec35efae5d63c7c9152b9d7e68d807465cf4
Mohammed Ameer <105409473+A-M-E-E-R@users.noreply.github.com>
2024-04-05 10:58:18 +0530

    Add files via upload

---
+++ Embedded C/Project/ADC/ADC1/main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: lcd.h
+.\main.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
+.\main.o: ADC.h
---
+++ Embedded C/Project/ADC/main.d
@@ -0,0 +1,6 @@
+.\main.o: main.c
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: C:\\Users\\IDEAPAD\\OneDrive\\Desktop\\projects\\Keil\\Header_Files\\lcd_4bit.h
+.\main.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
